a2e38e22dff22f1ac72521510e177b98928db5e6
Lindsay Wardell <three060@gmail.com>
2020-09-12 11:40:00 -0700

    Made home page mobile friendly

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,8 @@
   <Layout :pages="pages" activePage="/">
     <div class="hero overflow-hidden m-auto relative mt-8 shadow-lg">
       <CallToAction class="bg-hlc-yellow-500 text-gray-900">
-        <span class="font-bold">{{ content.action.main }}</span><br />
+        <span class="font-bold">{{ content.action.main }}</span
+        ><br />
         <span class="text-xs italic" role="label">{{
           content.action.subtitle
         }}</span>
@@ -14,7 +15,7 @@
         <nuxt-content :document="content" />
       </section>
       <div class="flex">
-        <section class="w-1/4 mr-3">
+        <section class="md:w-1/4 mr-3">
           <h3>Upcoming Events</h3>
           <img :src="events[0].image" alt="Event image" />
           <hr />
@@ -22,7 +23,7 @@
           <h5 class="text-left">When: {{ formatDate(events[0].date) }}</h5>
           <nuxt-content :document="events[0]" />
         </section>
-        <div class="w-3/4 ml-3">
+        <div class="md:w-3/4 ml-3">
           <section v-for="notice in notices" :key="notice.slug">
             <nuxt-content :document="notice" />
           </section>
