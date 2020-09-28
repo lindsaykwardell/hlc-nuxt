5d01dc04cc7e590487bdd0d048f3f2351d9f1ba7
Lindsay Wardell <three060@gmail.com>
2020-09-28 05:25:30 -0700

    Wrapped form in client only, adjustment to footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer
-    class="flex flex-col justify-center items-center mt-6 bg-hlc-dark-green-500 border-t-8 border-hlc-yellow-500 text-hlc-green-200"
+    class="flex flex-col justify-center items-center mt-6 bg-hlc-dark-green-500 border-t-8 border-hlc-yellow-500 text-hlc-green-100"
   >
     <div class="flex flex-col md:flex-row justify-around w-full py-4">
       <div class="md:w-1/4 lg:w-1/5 p-3 text-center md:text-left">
